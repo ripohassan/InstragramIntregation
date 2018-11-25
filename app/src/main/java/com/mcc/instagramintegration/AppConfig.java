d6e1d66040acfaa0ac31f4e507f4814cfad90173
ripohassan <ripohassan5@gmail.com>
2018-11-25 13:09:52 +0600

    init commit

---
+++ app/src/main/java/com/mcc/instagramintegration/AppConfig.java
@@ -0,0 +1,7 @@
+package com.mcc.instagramintegration;
+
+public class AppConfig {
+    public static final String CLIENT_ID = "fb4c44ec4bd24671a174f99c67954c91";
+    public static final String CLIENT_SECRET = "55cc1152c9b64c33b73adf5fac38d879";
+    public static final String CALLBACK_URL = "https://mcc.com.bd/user";
+}
---
+++ app/src/main/java/com/mcc/instagramintegration/MainActivity.java
@@ -0,0 +1,173 @@
+package com.mcc.instagramintegration;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private InstagramApp mApp;
+    private Button btnConnect, btnView;
+    private LinearLayout llAfterLoginView;
+    private HashMap<String, String> userInfoHashmap = new HashMap<String, String>();
+
+    private Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == InstagramApp.WHAT_FINALIZE) {
+                userInfoHashmap = mApp.getUserInfo();
+                Log.d("TAG", mApp.getUserName() + "");
+            } else if (msg.what == InstagramApp.WHAT_FINALIZE) {
+                Toast.makeText(MainActivity.this, "Check your network.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        initVars();
+        initListener();
+    }
+
+
+    private void initVars() {
+    }
+
+    private void initView() {
+
+        setContentView(R.layout.activity_main);
+
+        btnConnect = findViewById(R.id.btnConnect);
+        btnView = findViewById(R.id.btnView);
+        llAfterLoginView = findViewById(R.id.llafterloginView);
+        mApp = new InstagramApp(this, AppConfig.CLIENT_ID,
+                AppConfig.CLIENT_SECRET, AppConfig.CALLBACK_URL);
+        mApp.setListener(new InstagramApp.OAuthAuthenticationListener() {
+
+            @Override
+            public void onSuccess() {
+                btnConnect.setText("Disconnect");
+                llAfterLoginView.setVisibility(View.VISIBLE);
+                mApp.fetchUserName(handler);
+            }
+
+            @Override
+            public void onFail(String error) {
+                Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+
+        if (mApp.hasAccessToken()) {
+            // tvSummary.setText("Connected as " + mApp.getUserName());
+            btnConnect.setText("Disconnect");
+            llAfterLoginView.setVisibility(View.VISIBLE);
+            mApp.fetchUserName(handler);
+
+        }
+    }
+
+    private void initListener() {
+
+        btnConnect.setOnClickListener(this);
+        btnView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnConnect:
+                loginUser();
+                break;
+            case R.id.btnView:
+                displayPersonInfo();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void displayPersonInfo() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Profile Info");
+
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.profile_view, null);
+        builder.setView(view);
+
+        ImageView imvProfilePic = view.findViewById(R.id.imvProfilePic);
+        TextView tvName = view.findViewById(R.id.tvNofUserName);
+        TextView tvBio = view.findViewById(R.id.tvNoofBio);
+        TextView tvNoOfFollwers = view.findViewById(R.id.tvNoofFollower);
+        TextView tvNoOfFollowing = view.findViewById(R.id.tvNoofFollowing);
+        Glide.with(getApplicationContext())
+                .load(userInfoHashmap.get(InstagramApp.TAG_PROFILE_PICTURE))
+                .into(imvProfilePic);
+        tvBio.setText(userInfoHashmap.get(InstagramApp.TAG_BIO));
+        tvName.setText(userInfoHashmap.get(InstagramApp.TAG_USERNAME));
+        tvNoOfFollowing.setText(userInfoHashmap.get(InstagramApp.TAG_FOLLOWS));
+        tvNoOfFollwers.setText(userInfoHashmap
+                .get(InstagramApp.TAG_FOLLOWED_BY));
+        builder.create().show();
+
+
+    }
+
+    private void loginUser() {
+
+        if (mApp.hasAccessToken()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(
+                    MainActivity.this);
+            builder.setMessage("Disconnect from Instagram?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    mApp.resetAccessToken();
+                                    // btnConnect.setVisibility(View.VISIBLE);
+                                    llAfterLoginView.setVisibility(View.GONE);
+                                    btnConnect.setText("Connect");
+                                    // tvSummary.setText("Not connected");
+                                }
+                            })
+                    .setNegativeButton("No",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            final AlertDialog alert = builder.create();
+            alert.show();
+        } else {
+            mApp.authorize();
+        }
+    }
+}
